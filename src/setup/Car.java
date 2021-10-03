4b124e7cd083deb72c27cdfe6151d48c788ebc8f
rasul <leyla.najafzade.dc@gmail.com>
2021-10-03 15:20:22 -0500

    updating master to origin

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -1,20 +1,20 @@
 package setup;
 
 public class Car {
-<<<<<<< HEAD
-    //this is car class from master branch
 
-    // we will have conflict
 
-    //
+
 }
-=======
+
     /**
+     this is car class from master branch
+     we will have conflict
+
      * This is our car from james branch
      * more text
-     *
+
      * more text
+
      *
      */
-}
->>>>>>> james
+
