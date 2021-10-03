ec6b379fbad85864da7f615a25f800028342298e
rasul <leyla.najafzade.dc@gmail.com>
2021-10-03 11:47:15 -0500

    Added third file

---
+++ src/setup/Third.java
@@ -0,0 +1,11 @@
+package setup;
+
+public class Third {
+    public static void main(String[] args) {
+        /**create a new branch from master ( TC002 )
+         create a new file called third
+
+         *
+         */
+    }
+}
